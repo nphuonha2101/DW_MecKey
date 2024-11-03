0333189a74784b323fbed4b46b18ee52c7cc2642
Nguyen Phuong Nha <103343358+nphuonha2101@users.noreply.github.com>
2024-11-03 11:26:39 +0700

    fix: fix file log cannot get from db

--- model/file_log.py
+++ model/file_log.py
@@ -1,17 +1,12 @@
 class FileLog:
-    def __init__(self, id, id_config, status, time, file_path):
+    def __init__(self, id, id_config, status, time, file_path, description):
         self.id = id
         self.id_config = id_config
         self.status = status
         self.time = time
         self.file_path = file_path
+        self.description = description
 
     @classmethod
     def from_db(cls, result):
-        return cls(
-            id=result['id'],
-            id_config=result['id_config'],
-            status=result['status'],
-            time=result['time'],
-            file_path=result['file_path']
-        )
+        return cls(*result)
