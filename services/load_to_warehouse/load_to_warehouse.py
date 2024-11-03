8e4019b58c0e39506ce0535ef1af57b482358588
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-11-03 15:09:43 +0700

    fix: using create_file_log from abstract_service.py

--- services/load_to_warehouse/load_to_warehouse.py
+++ services/load_to_warehouse/load_to_warehouse.py
@@ -12,8 +12,8 @@ class LoadToWarehouse(AbstractService):
     def __init__(self, database_manager: DatabaseManager, event_bus: EventBus):
         super().__init__(database_manager, event_bus)
 
-    def check_load(self, feed_key):
-        file_log = self.get_file_log_by_status_and_feed_key(ServiceStatus.RL, feed_key)
+    def check_load(self, file_config):
+        file_log = self.get_file_log_by_status_and_feed_key(ServiceStatus.RL, file_config.feed_key)
         if file_log is not None:
             self.update_progress_to_ui(EventLevel.INFO, status_message.get(ServiceStatus.RL))
             return True;
@@ -21,28 +21,29 @@ class LoadToWarehouse(AbstractService):
             self.update_log_to_ui(EventLevel.ERROR, "No file log found for the given status and feed key.")
             return False;
 
-    def insert_status(self, feed_key, status):
-        file_config = self.get_file_config(feed_key)
-        try:
-            self.database_manager.connect_to_db(
-                os.getenv('CONTROL_DB_HOST'),
-                os.getenv('CONTROL_DB_USER'),
-                os.getenv('CONTROL_DB_PASSWORD'),
-                os.getenv('CONTROL_DB_NAME')
-            )
+    # def insert_status(self, feed_key, status):
+    #
+    #     try:
+    #         self.database_manager.connect_to_db(
+    #             os.getenv('CONTROL_DB_HOST'),
+    #             os.getenv('CONTROL_DB_USER'),
+    #             os.getenv('CONTROL_DB_PASSWORD'),
+    #             os.getenv('CONTROL_DB_NAME')
+    #         )
+    #
+    #         self.database_manager.call_procedure('insert_file_log', (status.value, file_config.source_url, file_config.folder_data_path, status_message.get(status)))
+    #         self.update_progress_to_ui(EventLevel.INFO, status_message.get(status))
+    #
+    #     except Exception as e:
+    #         self.update_log_to_ui(EventLevel.ERROR, f"Error getting file config: {e}")
+    #         raise RuntimeError(e)
+    #     finally:
+    #         self.database_manager.close_connection()
 
-            self.database_manager.call_procedure('insert_file_log', (status.value, file_config.source_url, file_config.folder_data_path, status_message.get(status)))
-            self.update_progress_to_ui(EventLevel.INFO, status_message.get(status))
+    def load_data(self, file_config):
 
-        except Exception as e:
-            self.update_log_to_ui(EventLevel.ERROR, f"Error getting file config: {e}")
-            raise RuntimeError(e)
-        finally:
-            self.database_manager.close_connection()
-
-    def load_data(self, feed_key):
-
-        self.insert_status(feed_key, ServiceStatus.LX)
+        self.create_file_log(file_config.id, ServiceStatus.LX, file_config.folder_data_path)
+        self.update_progress_to_ui(EventLevel.INFO, status_message.get(ServiceStatus.LX))
 
         try:
             self.database_manager.connect_to_db(
@@ -65,10 +66,15 @@ class LoadToWarehouse(AbstractService):
 
     def run(self):
         feed_key = "akko_feed"
-        is_ready = self.check_load(feed_key)
+        file_config = self.get_file_config(feed_key)
+
+        is_ready = self.check_load(file_config)
+
         if (is_ready):
-            is_success = self.load_data(feed_key)
+            is_success = self.load_data(file_config)
             if (is_success):
-                self.insert_status(feed_key, ServiceStatus.SL)
+                self.create_file_log(file_config.id, ServiceStatus.SL, file_config.folder_data_path)
+                self.update_progress_to_ui(EventLevel.SUCCESS, status_message.get(ServiceStatus.SL))
             else:
-                self.insert_status(feed_key, ServiceStatus.FL)
+                self.create_file_log(file_config.id, ServiceStatus.FL, file_config.folder_data_path)
+                self.update_progress_to_ui(EventLevel.ERROR, status_message.get(ServiceStatus.FL))
